86961e767df6f74c3206ded8379c961c920119e4
icevct <icevct@gmail.com>
2018-03-20 14:33:17 -0300

    funcao que gera matriz de confusao adicionada e codigo pronto

--- projeto1.m
+++ projeto1.m
@@ -45,14 +45,48 @@ respostasTeste(numPorClasseTeste*2 + 1 : numPorClasseTeste*3, :) = respostas(num
 
 
 %% Criando os modelos para o treinamento usando o metodo Knn
-
-% K = 1
 % O standardize eh para padronizar as escalas, de forma a nao atrapalhar no
 % treinamento e consequentemente na predicao
 
-% Trenamento com K = 1
+% Treinamento com K = 1
 modeloK1 = fitcknn(medidasTreino, respostasTreino, 'NumNeighbors', 1, 'Standardize',1);
 % Predicao com K = 1
 respostaK1 = predict(modeloK1, medidasTeste);
 
+% Treinamento com K = 5
+modeloK5 = fitcknn(medidasTreino, respostasTreino, 'NumNeighbors', 5, 'Standardize',1);
+% Predicao com K = 5
+respostaK5 = predict(modeloK5, medidasTeste);
+
+% Treinamento com K = 10
+modeloK10 = fitcknn(medidasTreino, respostasTreino, 'NumNeighbors', 10, 'Standardize',1);
+% Predicao com K = 10
+respostaK10 = predict(modeloK10, medidasTeste);
+
+% Treinamento com K = 30
+modeloK30 = fitcknn(medidasTreino, respostasTreino, 'NumNeighbors', 30, 'Standardize',1);
+% Predicao com K = 30
+respostaK30 = predict(modeloK30, medidasTeste);
+
+% Treinamento com K = 50
+modeloK50 = fitcknn(medidasTreino, respostasTreino, 'NumNeighbors', 50, 'Standardize',1);
+% Predicao com K = 50
+respostaK50 = predict(modeloK50, medidasTeste);
+
+%% Gerando e imprimindo as matrizes de confusao para cada K usado para treinar o Knn
+
+%K = 1
+geraMatConf(respostasTeste, respostaK1, medidasTeste);
+
+%K = 5
+geraMatConf(respostasTeste, respostaK5, medidasTeste);
+
+%K = 10
+geraMatConf(respostasTeste, respostaK10, medidasTeste);
+
+%K = 30
+geraMatConf(respostasTeste, respostaK30, medidasTeste);
+
+%K = 50
+geraMatConf(respostasTeste, respostaK50, medidasTeste);
 
